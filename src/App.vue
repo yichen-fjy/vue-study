ebe69465873a56f444ee0219bad3efc4819c60a0
fjy <fanjingyao@qq.com>
2020-03-05 19:47:09 +0800

    初次使用vue框架

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" ref="img_node">
+    <div v-text='html'></div>
+    <div v-html='html'></div>
+    <div v-show='isShow'>显示隐藏</div>
+    <button @click='isShow=!isShow'>点击切换</button>
+    <ul>
+      <li v-for="(item,index) in arr" :key="index">{{ item.name }}</li>
+    </ul>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      msg:'Hello',
+      html: '<a href="https://baidu.com">百度</a>',
+      isShow: true,
+      arr: [{ name: 'a' },{ name: 'b' }]
+    };
+  },
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,19 +95,20 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
+  li {
   display: inline-block;
   margin: 0 10px;
+    a {
+      color: #42b983;
+    }
+  }
 }
-a {
-  color: #42b983;
-}
+
 </style>
